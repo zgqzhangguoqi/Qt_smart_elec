6a0e7a6c57bfd4659063b8f1051666cf4311285c
ZGQ0419 <2393421983@qq.com>
2018-07-16 10:23:48 +0800

    first

---
+++ mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<QMouseEvent>
+#include<QPaintEvent>
+#include<QtGui>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QImage image;
+     void Paint();
+protected:
+    /*添加自定义方法，paintEvent（）
+      drawImage：设置显示位置
+     */
+    void paintEvent(QPaintEvent *){
+        QPainter painter(this);
+        painter.drawImage(40,40,image);
+    }
+
+};
+
+#endif // MAINWINDOW_H
